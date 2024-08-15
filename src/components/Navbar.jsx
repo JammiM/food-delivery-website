e1175b9cc54a3ce3421a45a2e750ffd616208846
James Morrissey <jammim19@gmail.com>
2024-08-15 11:27:02 +0100

    Added a layout for the website name and delivery service

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { AiOutlineMenu } from "react-icons/ai";
+
 const Navbar = () => {
   return (
-    <div>
-      <div>
-        <AiOutlineMenu size={30} />
+    <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
+      <div className="flex items-center">
+        <div className="cursor-pointer">
+          <AiOutlineMenu size={30} />
+        </div>
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+          Best <span className="font-bold">Eats</span>
+        </h1>
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-black text-white rounded-full p-2">Delivery</p>
+          <p className="p-2">Pick up</p>
+        </div>
       </div>
-      <h1>
-        Best <span>Eats</span>
-      </h1>
     </div>
   );
 };
