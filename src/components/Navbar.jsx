933b31f02851e31c5a7460e6b02404299e10c489
James Morrissey <jammim19@gmail.com>
2024-08-15 18:40:08 +0100

    Added the search input field

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 
 const Navbar = () => {
   return (
@@ -8,13 +8,24 @@ const Navbar = () => {
         <div className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
+
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
           Best <span className="font-bold">Eats</span>
         </h1>
+
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
           <p className="bg-black text-white rounded-full p-2">Delivery</p>
           <p className="p-2">Pick up</p>
         </div>
+      </div>
+
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px]">
+        <AiOutlineSearch size={25} />
+        <input
+          type="text"
+          placeholder="Search here"
+          className="bg-transparent p-1 w-full focus:outline-none"
+        />
       </div>
     </div>
   );
