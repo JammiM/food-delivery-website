6d12eb373bacd18437def59fff11976b734f0b82
James Morrissey <jammim19@gmail.com>
2024-09-16 15:59:22 +0100

    Defining the side menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 import { BsFillCartFill } from "react-icons/bs";
+import { TbTruckDelivery } from "react-icons/tb";
 
 const Navbar = () => {
   return (
@@ -33,6 +34,27 @@ const Navbar = () => {
         <BsFillCartFill size={20} className="mr-2" />
         <span className="font-bold">Cart</span>
       </button>
+
+      {/* Side menu overlay */}
+      <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+
+      <div className="fixed top-0 w-[300px] h-screen bg-white z-10 left-0 duration-300">
+        <AiOutlineClose
+          size={30}
+          className="absolute right-4 top-4 cursor-pointer"
+        />
+        <h2 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+          Best <span className="font-bold">Eats</span>
+        </h2>
+        <nav>
+          <ul>
+            <li className="text-xl py-4 flex">
+              <TbTruckDelivery size={25} className="mr-4" />
+              Order
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
