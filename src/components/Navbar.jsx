ada0da9a90b55a7f0102fc6ad254a205c1698cdb
James Morrissey <jammim19@gmail.com>
2025-02-07 11:03:10 +0000

    Added small fixes to some classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,28 +37,32 @@ const Navbar = () => {
         />
       </div>
 
-      <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full">
-        <BsFillCartFill size={20} className="mr-2" />
-        <span className="font-bold">Cart</span>
-      </button>
-
+      <div>
+        <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full">
+          <BsFillCartFill size={20} className="mr-2" />
+          Cart
+        </button>
+      </div>
       {/* Side menu overlay */}
       <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
 
-      <div className="fixed top-0 w-[300px] h-screen bg-white z-10 left-0 duration-300">
+      <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
         <AiOutlineClose
           size={30}
           className="absolute right-4 top-4 cursor-pointer"
         />
-        <h2 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+
+        <h2 className="text-2xl p-4">
           Best <span className="font-bold">Eats</span>
         </h2>
+
         <nav>
-          <ul className="flex flex-col p-4 text-grey-800">
+          <ul className="flex flex-col p-4 text-gray-800">
             <li className="text-xl py-4 flex">
               <TbTruckDelivery size={25} className="mr-4" />
-              Order
+              Orders
             </li>
+
             <li className="text-xl py-4 flex">
               <MdFavorite size={25} className="mr-4" />
               Favorites
