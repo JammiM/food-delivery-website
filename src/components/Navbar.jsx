5fbcdf7a67754c6b684c686fb6a25f537bd47886
James Morrissey <jammim19@gmail.com>
2025-02-07 15:21:04 +0000

    Added functionality for the side menu to be accessible

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AiFillTag,
   AiOutlineClose,
@@ -11,10 +11,11 @@ import { MdHelp, MdFavorite } from "react-icons/md";
 import { FaUserFriends, FaWallet } from "react-icons/fa";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
 
@@ -43,13 +44,27 @@ const Navbar = () => {
           Cart
         </button>
       </div>
-      {/* Side menu overlay */}
-      <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      {/* Side menu dark overlay */}
 
-      <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
+      {nav ? (
+        <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      ) : (
+        ""
+      )}
+      {/* Side menu */}
+      <div
+        className={
+          nav
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
+      >
         <AiOutlineClose
           size={30}
           className="absolute right-4 top-4 cursor-pointer"
+          onClick={() => {
+            setNav(!nav);
+          }}
         />
 
         <h2 className="text-2xl p-4">
